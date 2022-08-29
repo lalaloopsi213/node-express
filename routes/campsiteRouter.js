a942c006dee87394ce27f6d99d81786b7a0b0420
lalaloopsi213 <fg1041@gmail.com>
2022-08-28 20:32:33 -0500

    node JS, Express and MongoDB Week 1 workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,24 +1,58 @@
 const express = require('express');
 const campsiteRouter = express.Router();
 
+
 campsiteRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
-})
-.get((req, res) => {
+});
+
+app.get('/campsites', (req, res) => {
     res.end('Will send all the campsites to you');
-})
-.post((req, res) => {
+});
+
+app.post('/campsites', (req, res) => {
     res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
+});
+
+app.put('/campsites', (req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
-})
-.delete((req, res) => {
+});
+
+app.delete('/campsites', (req, res) => {
     res.end('Deleting all campsites');
 });
 
-module.exports = campsiteRouter;
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
